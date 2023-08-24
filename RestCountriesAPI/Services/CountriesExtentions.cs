d1724f72d25d6785ee0e75304210062369722b9a
BohdanBuhrii <bobuhrii@gmail.com>
2023-08-24 19:44:50 +0200

    UC#1 Create filter by population function

--- RestCountriesAPI/Services/CountriesExtentions.cs
+++ RestCountriesAPI/Services/CountriesExtentions.cs
@@ -1,10 +1,17 @@
-﻿using System.Text.Json;
-using System.Text.Json.Nodes;
+﻿using System.Text.Json.Nodes;
 
 namespace RestCountriesAPI.Services
 {
     public static class CountriesExtentions
     {
+        /// <summary>
+        /// Filters values by country's 'name/common'. 
+        /// The filter searches for countries names that contains a string.
+        /// The filter is case insensitive.
+        /// </summary>
+        /// <param name="countries">Array of country nodes.</param>
+        /// <param name="nameSearch">Search term.</param>
+        /// <returns>Filtered array of country nodes.</returns>
         public static IEnumerable<JsonNode?> FilterByName(this IEnumerable<JsonNode?> countries, string nameSearch)
             => countries.Where(x =>
             {
@@ -18,5 +25,23 @@ namespace RestCountriesAPI.Services
 
                 return common.ToString().ToLower().Contains(nameSearch.ToLower());
             });
+
+        /// <summary>
+        /// Filters values by country's 'population'. 
+        /// </summary>
+        /// <param name="countries">Array of country nodes.</param>
+        /// <param name="maxPopulation">Maximum population in millions.</param>
+        /// <returns>Countries with population less than provided number.</returns>
+        public static IEnumerable<JsonNode?> FilterByPopulation(this IEnumerable<JsonNode?> countries, int maxPopulation)
+            => countries.Where(x =>
+            {
+                if (x == null) return false;
+
+                var population = x["population"];
+                if (population is null) return false;
+
+                return population.GetValue<int>() < maxPopulation * 10e6;
+            });
+
     }
 }
